b824863fb392a365cb7fb9a786084015dc290a4f
juwon <juwon49290819@gmali.com>
2026-07-04 16:29:40 +0900

    initial project setup

---
+++ DevTask_Backend/src/main/java/com/example/devtask_backend/DevTaskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.devtask_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTaskBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevTaskBackendApplication.class, args);
+    }
+
+}
---
+++ DevTask_Backend/src/test/java/com/example/devtask_backend/DevTaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devtask_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTaskBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
